1d8ba9ddf8b9a5e9d79e715bfb6779f95f10e52b
Venarge0411 <leonardojmarinellib09@gmail.com>
2022-04-03 13:23:27 -0400

    Id Data

---
+++ data/secrets/IdData.java
@@ -0,0 +1,19 @@
+package secrets;
+
+import org.apache.log4j.helpers.Loader;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import sun.security.util.RegisteredDomain;
+
+import java.rmi.registry.Registry;
+
+public class IdData {
+
+    private static final Logger LOGGER = LogManager.getLogger ();
+
+    private void preInit(){}
+
+    private void init(){}
+
+    private void postInit(){}
+}
